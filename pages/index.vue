e3ca68840126cd552a5add0f22265bbce13dc942
farosFreed <jessica.divers@gmail.com>
2026-07-17 09:33:41 -0700

    fix: search for production

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,44 @@
 <script lang="ts" setup>
+import MiniSearch from 'minisearch'
 // import searchContent from
 // Create a query looking for anything in content/ directory
 const { data } = await useAsyncData("home", () => queryContent().find());
 // Search the data
-const miniSearchOptions = defineMiniSearchOptions({
-  fields: ["title"],
+const search = ref('');
+const miniSearch = new MiniSearch({
+  idField: '_path', // we don't have an id field in the data, so we use the path as the id
+  fields: ['title'],
+  storeFields: ['title', '_path'],
+  searchOptions: {
+    fuzzy: 0.2,
+    prefix: true,
+    boost: {
+      title: 2,
+    },
+  },
 });
-const search = ref("");
-const results = ref([]);
+miniSearch.addAll(data.value ?? []);
+
+const filtered = computed(() => {
+  console.log(data.value);
+  if (!search.value) return data.value;
+
+  // search for items in miniSearch and return a set of ids
+  const ids = new Set(miniSearch.search(search.value).map(item => item._path))
+  // return the items in data.value that have the ids
+  return (data.value ?? []).filter(item => ids.has(item._path))
+  // working with miniSearch data
+  // let resultsLocal = miniSearch.search(search.value);
+  // console.log(resultsLocal);
+  // return resultsLocal;
+});
 </script>
 <template>
   <main>
     <h1>Cafe de Oleson Menu</h1>
     <input v-model="search" placeholder="search" />
-    <ContentList :query="{ where: { title: { $regex: `/${search}/ig` } } }">
-      <template #default="{ list }">
-        <MenuItemPreview :items="list" />
-      </template>
-      <template #not-found>
-        <p v-if="search.length > 0">
-          Nothing found for '{{ search }}'. Browse recipes below:
-        </p>
-        <MenuItemPreview v-if="data" :items="data" />
-      </template>
-    </ContentList>
+    <MenuItemPreview v-if="filtered.length > 0" :items="filtered" />
+    <p v-else>No items found</p>
   </main>
 </template>
 <style lang="scss" scoped>
